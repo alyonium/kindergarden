862bc7499450858f030c81d4d1469219f37285c8
Alona Ivanova <alyonium2.0@gmail.com>
2020-08-20 19:22:33 +0300

    1,2 (adapt don't end)

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,32 @@
 $(document).ready(function () {
-    let isAnimate = false;
-    $('.header__name').hover(function() {
-        if (!isAnimate) {
-            $(this).toggleClass('animate__animated animate__bounce')
-            isAnimate = true;
-            setTimeout(() => isAnimate = false, 1000)
-        }
-    });
+
+    //h1 animation
+    if ($(window).width() >= 992) {
+        let isAnimate = false;
+        $('.header_main').hover(function() {
+            if (!isAnimate) {
+                $(this).toggleClass('animate__animated animate__bounce')
+                isAnimate = true;
+                setTimeout(() => isAnimate = false, 1000)
+            }
+        });
+    }
+
+    //first slide parallax
+    $('.slide1').paroller();
+
+    //cameras moving
+    if ($(window).width() >= 992) {
+        const scene = $('.scene')[0];
+        const parallaxFirst = new Parallax(scene);
+
+        const scene2 = $('.scene')[1];
+        const parallaxSecond = new Parallax(scene2);
+    }
+
+
+
 })
+
+// var scene = document.getElementById('scene');
+// var parallaxInstance = new Parallax(scene);
